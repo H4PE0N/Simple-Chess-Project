eb9afabd21175e5e9f3a80253e3769cf9def4587
Hampus Fridholm <hampus.fridholm@gmail.com>
2021-09-24 09:50:33 +0200

    I have fixed a bug (typo) that displayed the wrong winning team. The value was inverted. But now that is fixed, and the game is very fun to play.

--- Source-Program-Folder/Game-Program-Folder/Source-Program-Folder/game-help-functions.c
+++ Source-Program-Folder/Game-Program-Folder/Source-Program-Folder/game-help-functions.c
@@ -5,7 +5,7 @@ bool display_game_result(Board board, Team winner)
 {
 	if(!display_chess_board(board)) return false;
 
-	if(!piece_team_exists(winner))
+	if(piece_team_exists(winner))
 	{
 		char teamString[20] = "\0";
 		if(!chess_team_string(teamString, winner)) return false;
